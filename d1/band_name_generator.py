1a63e8d908ab474a41ad4b02a2795853dcb2ba60
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-01-27 10:56:13 +0000

    Sat, Jan 27, 2024, 10:56 AM +00:00

---
+++ d1/band_name_generator.py
@@ -0,0 +1,4 @@
+print("This is a band name generator.")
+town = input("What town did you grow up in?\n")
+pet = input("What is the name of your first per?\n")
+print(f"\nYour band could be called {town} {pet}.\n")
