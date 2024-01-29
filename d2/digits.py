29f9080b05be15b32f64efd54d9197c839bb5083
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-01-29 22:02:31 +0000

    Mon, Jan 29, 2024, 10:02 PM +00:00

--- d2/digits.py
+++ d2/digits.py
@@ -1,5 +1,6 @@
-two_digit_number = input()
+two_digit_number = input("digits: ")
 ###########do not change line 1 code#########
 print(type(two_digit_number))
-#total = int(two_digit_number[0]) + two_digit_number[1]
-#print(total)
+total = int(two_digit_number[0]) + int(two_digit_number[1])
+print(type(total))
+print(total)
