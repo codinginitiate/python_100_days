978d3e82e6a3789119ef283bfb1f83f4999d4d6c
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-07 17:22:18 +0000

    Wed, Feb 7, 2024, 5:22 PM +00:00

--- d2/tip_calculator.py
+++ d2/tip_calculator.py
@@ -6,5 +6,5 @@ Thus everyone's share of the total bill is $30.00 plus a $3.60 tip.
 Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
 '''
 print("Welcom to the tip calculator.")
-bill = input("What was the total bill? ").strip().replace("$", "")
-print(bill)
+bill = float(input("What was the total bill? ").strip().replace("$", ""), 2)
+print(type(bill))
