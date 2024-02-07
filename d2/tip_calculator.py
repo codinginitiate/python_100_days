875b984019aeb753c99e3bdc0520ec440dd87b09
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-07 18:03:53 +0000

    Wed, Feb 7, 2024, 6:03 PM +00:00

--- d2/tip_calculator.py
+++ d2/tip_calculator.py
@@ -6,8 +6,13 @@ Thus everyone's share of the total bill is $30.00 plus a $3.60 tip.
 Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
 '''
 print("Welcome to the tip calculator.")
+# make bill a float and remove $ if used in input
 bill = float(input("What was the total bill? ").strip().replace("$", ""))
+# determine tip percentage and remove % sign if used in input
 tip = float(input("What percentage would you like to tip? 10, 12,15? ").strip().replace("%", ""))/100
+# make sure number of people entered a whole number
 num_people = int(input("How many people to split the bill? "))
+# calculate the amount each person shoule pay
 amount_to_pay = (bill*(tip+1)/num_people)
+# print amount to py rounded to two decimal places
 print(f"Each person should pay: ${amount_to_pay:.2f}")
