2318fbc0d6378559098a1092fdde3d4f25437061
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-15 17:23:03 +0000

    Thu, Feb 15, 2024, 5:23 PM +00:00

--- d4/rock_paper_scissors.py
+++ d4/rock_paper_scissors.py
@@ -33,11 +33,11 @@ my_choice = input("What is your choice (1)rock , (2)paper, or (3)scissors? Enter
 print(set[int(my_choice)-1])
 computer = random.choice(set)
 print(computer)
-if set[int(my_choice)-1] == 'rock' and computer == 'scissors':
+if set[int(my_choice)-1] == rock and computer == scissors:
     print("YOU WIN!")
-elif set[int(my_choice)-1] == 'scissors' and computer == 'paper':
+elif set[int(my_choice)-1] == scissors and computer == paper:
     print("YOU WIN!")
-elif set[int(my_choice)-1] == 'paper' and computer == 'rock':
+elif set[int(my_choice)-1] == paper and computer == rock:
     print("YOU WIN!")
 elif set[int(my_choice)-1] == computer:
     print("It's a tie.")
