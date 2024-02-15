4bc3a24cbcb7d30a41cf66f3a14dfabaac07ce28
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-15 20:03:16 +0000

    Thu, Feb 15, 2024, 8:03 PM +00:00

--- d5/password_generator.py
+++ d5/password_generator.py
@@ -11,7 +11,8 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
-for num in range(nr_letters)
+for _ in range(nr_letters):
+    print(random.choice(letter))
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
