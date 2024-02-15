792375dda6bedf60d313dcaebea90fdb61e789c5
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-15 21:03:32 +0000

    Thu, Feb 15, 2024, 9:03 PM +00:00

--- d5/password_generator.py
+++ d5/password_generator.py
@@ -28,8 +28,9 @@ for _ in range(nr_symbols):
     rand_list.append(random.choice(symbols))
 for _ in range(nr_numbers):
     rand_list.append(random.choice(numbers))
-for i in range(rand_list):
- print(random.shuffle(rand_list))
+print(rand_list)
+for i in range(len(rand_list)):
+    print(rand_list[i])
 '''
 for i in range(len([rand_list])):
     print(i, end="")
