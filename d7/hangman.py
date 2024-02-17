cb12e19eba95d194ebbd512ae30fd2124507eda4
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-17 13:43:16 +0000

    Sat, Feb 17, 2024, 1:43 PM +00:00

--- d7/hangman.py
+++ d7/hangman.py
@@ -7,8 +7,8 @@ chosen_word = random.choice(word_list)
 for i in range(6):
   guessed_letter=input("Guess a letter: ").lower()
 
-  for letter in chosen_word:
+for letter in chosen_word:
     if guessed_letter == letter:
-      print("Right")
+        print("Right")
     else:
-      print("Wrong")
+        print("Wrong")
