7b779d0e80e3e46f2cf1856a9344dd888b4ec7c5
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-17 14:39:47 +0000

    Sat, Feb 17, 2024, 2:39 PM +00:00

--- d7/hangman.py
+++ d7/hangman.py
@@ -9,7 +9,7 @@ for letter in chosen_word:
     display.append("_")
 
 guessed_letter=input("Guess a letter: ").lower()
-for position in range(word_length)
+for position in range(word_length):
     for letter in chosen_word:
         if guessed_letter == letter:
             display[position] = guessed_letter
