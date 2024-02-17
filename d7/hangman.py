25227d0c0729f2ecb9c9edc355240467f2b0be25
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-17 13:49:31 +0000

    Sat, Feb 17, 2024, 1:49 PM +00:00

--- d7/hangman.py
+++ d7/hangman.py
@@ -4,7 +4,9 @@ word_list = ["aardvark", "baboon", "camel"]
 display = []
 
 chosen_word = random.choice(word_list)
-
+for letter in len(chosen_word):
+    dispay.append("_")
+    
 guessed_letter=input("Guess a letter: ").lower()
 for letter in chosen_word:
     if guessed_letter == letter:
