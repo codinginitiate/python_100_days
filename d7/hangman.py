6bdde5e05503385a2af6e9b406e655725e8248bf
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-17 16:45:19 +0000

    Sat, Feb 17, 2024, 4:45 PM +00:00

--- d7/hangman.py
+++ d7/hangman.py
@@ -7,23 +7,21 @@ chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 for letter in chosen_word:
     display.append("_")
+while "_" in display:
+    guessed_letter = input("Guess a letter: ").lower()
+    '''
+    i=0
+    for letter in chosen_word:
+    i += 1
+    if guess == letter:
+        display[i-1] = guess
+    '''
+    # above code block can be used for below code block
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if guessed_letter == letter:
+            display[position] = guessed_letter
 
-guessed_letter = input("Guess a letter: ").lower()
-'''
-i=0
-for letter in chosen_word:
-  i += 1
-  if guess == letter:
-    display[i-1] = guess
-for position in range(word_length):
-    letter = chosen_word[position]
-    if guessed_letter == letter:
-        display[position] = guessed_letter
-'''
-# above code block can be used for this code block
-for position in range(word_length):
-    letter = chosen_word[position]
-    if guessed_letter == letter:
-        display[position] = guessed_letter
-print(display)
+    print(display)
 
+print("You win!")
