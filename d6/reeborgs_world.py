62ffe03dabcbcb8fd9892ae878dfcc137567b6c1
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-17 00:15:50 +0000

    Sat, Feb 17, 2024, 12:15 AM +00:00

---
+++ d6/reeborgs_world.py
@@ -0,0 +1,30 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def face_north():
+    while not is_facing_north():
+        turn_left()
+
+def face_east():
+    face_north()
+    turn_right()
+
+def face_west():
+    face_north()
+    turn_left()
+
+def face_south():
+    face_north()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if front_is_clear():
+        move()
+    elif right_is_clear():
+        turn_right()
+        move()
+    elif turn_left():
+        move()
