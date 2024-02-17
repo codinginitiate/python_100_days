7b2017d627fe6a17bb730baaa4f6d1356e6071f2
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-17 15:19:07 +0000

    Sat, Feb 17, 2024, 3:19 PM +00:00

--- d7/hangman.py
+++ d7/hangman.py
@@ -9,10 +9,11 @@ for letter in chosen_word:
     display.append("_")
 
 guessed_letter=input("Guess a letter: ").lower()
+'''
 for position in range(word_length):
-    for letter in chosen_word:
-        if guessed_letter == letter:
-            display[position] = guessed_letter
+    letter = chosen_word[position]
+    if guessed_letter == letter:
+        display[position] = guessed_letter
 
 
 print(display)
