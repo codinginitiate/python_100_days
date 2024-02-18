aff5d8017c5bc017c820fa5db22ef3e98634d9f5
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-18 01:09:29 +0000

    Sun, Feb 18, 2024, 1:09 AM +00:00

--- d7/hangman.py
+++ d7/hangman.py
@@ -86,6 +86,9 @@ for letter in chosen_word:
 
 while not end_of_game:
     guessed_letter = input("Guess a letter: ").lower()
+
+    if guessed_letter in guesses:
+
     '''
     i=0
     for letter in chosen_word:
@@ -100,7 +103,7 @@ while not end_of_game:
             display[position] = guessed_letter
 
     print(display)
-    guesses += guessed_letter
+
     print(f"\n The letters you have used: {guesses}")
 
     if guessed_letter not in chosen_word:
