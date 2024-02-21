8956eba4c032ebc6847189f8546736db9867e9aa
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-21 02:37:31 +0000

    Wed, Feb 21, 2024, 2:37 AM +00:00

--- d7/hangman.py
+++ d7/hangman.py
@@ -23,6 +23,9 @@ while not end_of_game:
     if guessed_letter in guesses:
         print("You have used that letter already. Choose again.")
         pass
+    if guessed_letter not in guesses:
+        guesses += guessed_letter
+        pass
 
     '''
     i=0
