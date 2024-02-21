e930bb10ceba35eb79e96a21feb587554827aaa4
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-21 06:12:22 +0000

    Wed, Feb 21, 2024, 6:12 AM +00:00

--- d8/notes8.py
+++ d8/notes8.py
@@ -6,7 +6,7 @@ def prime_checker(number):
     if is_prime:
         print("It's a prime number.")
     else:
-        print(It's not a prime number.")
+        print("It's not a prime number.")
 
 n = int(input())
 prime_checker(number=n)
