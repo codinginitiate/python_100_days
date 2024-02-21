f0529cc21c5276b7f26b84ee71bf61d1d14955bd
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-21 15:09:53 +0000

    Wed, Feb 21, 2024, 3:09 PM +00:00

--- d9/caesar_cipher.py
+++ d9/caesar_cipher.py
@@ -1,16 +1,18 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-position_1 = []
+position_1 = ""
 '''
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 '''
-text = "civilization"
+text = "hello"
+shift = 3
 def encrypt(text):
     for letter in text:
         for i in range(0,26):
             if letter == alphabet[i]:
-                position_1.append(i)
+                new_letter = alphabet[i+shift]
+                position_1 += new_letter
 
 
 
