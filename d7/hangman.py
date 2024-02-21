d72731301183b413c8b1a2b46ec9b2b7553d8fae
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-21 02:15:53 +0000

    Wed, Feb 21, 2024, 2:15 AM +00:00

--- d7/hangman.py
+++ d7/hangman.py
@@ -45,9 +45,10 @@ while not end_of_game:
 
     print(f"\n The letters you have used: {', '.join(guesses)}")
 
-    if guessed_letter not in chosen_word and guessed_letter not in guesses:
-        lives -= 1
-    print(hangman_art.stages[lives])
+    if guessed_letter not in guesses:
+        if guessed_letter not in chosen_word:
+            lives -= 1
+        print(hangman_art.stages[lives])
 
     if "_" not in display:
         end_of_game = True
