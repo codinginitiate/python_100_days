c51f0d5d1e6d2e7b42d0226d72a0401edb659884
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-21 14:09:00 +0000

    Wed, Feb 21, 2024, 2:09 PM +00:00

--- d9/caesar_cipher.py
+++ d9/caesar_cipher.py
@@ -4,4 +4,9 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt
+def encrypt(text, shift):
+    for letter in text:
+        if letter in alphbet:
+            print(letter.positio)
+
+
