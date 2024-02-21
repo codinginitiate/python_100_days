a2451fe67658e01a3a1e0778e0be53ce6314ba26
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-21 02:24:53 +0000

    Wed, Feb 21, 2024, 2:24 AM +00:00

--- d7/hangman.py
+++ d7/hangman.py
@@ -23,38 +23,38 @@ while not end_of_game:
     if guessed_letter in guesses:
         print("You have used that letter already. Choose again.")
         pass
-        if guessed_letter not in guesses:
-            guesses += guessed_letter
-        else:
-            pass
+    if guessed_letter not in guesses:
+        guesses += guessed_letter
+    else:
+        pass
 
-        '''
-        i=0
-        for letter in chosen_word:
-        i += 1
-        if guess == letter:
-            display[i-1] = guess
-        '''
-        # above code block can be used for below code block
-        for position in range(word_length):
-            letter = chosen_word[position]
-            if guessed_letter == letter:
-                display[position] = guessed_letter
+    '''
+    i=0
+    for letter in chosen_word:
+    i += 1
+    if guess == letter:
+        display[i-1] = guess
+    '''
+    # above code block can be used for below code block
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if guessed_letter == letter:
+            display[position] = guessed_letter
 
-        print(f"{' '.join(display)}")
+    print(f"{' '.join(display)}")
 
-        print(f"\n The letters you have used: {', '.join(guesses)}")
+    print(f"\n The letters you have used: {', '.join(guesses)}")
 
-        if guessed_letter not in chosen_word:
-            lives -= 1
-        print(hangman_art.stages[lives])
+    if guessed_letter not in chosen_word:
+        lives -= 1
+    print(hangman_art.stages[lives])
 
-        if "_" not in display:
-            end_of_game = True
-            print("You win!")
+    if "_" not in display:
+        end_of_game = True
+        print("You win!")
 
-        if lives == 0:
-            end_of_game = True
-            print("You lose.")
+    if lives == 0:
+        end_of_game = True
+        print("You lose.")
 
 
