37bba5d17f599042ba4b45990ffba6ef6fe1efe1
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-22 04:15:06 +0000

    Thu, Feb 22, 2024, 4:15 AM +00:00

--- d8/caesar_cipher.py
+++ d8/caesar_cipher.py
@@ -6,49 +6,13 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 # argument for parameter shift_amount
 shift = int(input("Type the shift number:\n"))
-# function to encode message
-def encrypt(plain_text, shift_amount):
-    new_text = ""
-    for letter in plain_text:
-        if letter in alphabet:
-            index = alphabet.index(letter)
-            new_index = (index + shift_amount) % 26
-            new_letter = alphabet[new_index]
-            new_text += new_letter
-        else:
-            # If the character is not a letter (e.g., space or punctuation), leave it unchanged.
-            new_text += letter
-    return new_text
-# function to decode message
-def decrypt(plain_text, shift_amount):
-    new_text = ""
-    for letter in plain_text:
-        if letter in alphabet:
-            index = alphabet.index(letter)
-            new_index = (index - shift_amount) % 26
-            new_letter = alphabet[new_index]
-            new_text += new_letter
-        else:
-            # If the character is not a letter (e.g., space or punctuation), leave it unchanged.
-            new_text += letter
-    return new_text
-# determine which function to use
-if direction == "encode":
-    message = encrypt(plain_text = text, shift_amount = shift)
-elif direction == "decode":
-    message = decrypt(plain_text = text, shift_amount = shift)
-# if direction is wrong
-else:
-    message = "Sorry, try again."
-# print returned encoded/decoded message
-print(message)
 
-def ceasar_cipher(coding_direction, plain_text, shift):
+def ceasar_cipher(coding_direction, plain_text, shift_amount):
     new_text = ""
     for letter in plain_text:
         if letter in alphabet:
             index = alphabet.index(letter)
-            if direction == "encode":
+            if coding_direction == "encode":
                 new_index = (index + shift_amount) % 26
             else:
                 new_index = (index - shift_amount) % 26
@@ -58,3 +22,6 @@ def ceasar_cipher(coding_direction, plain_text, shift):
             # If the character is not a letter (e.g., space or punctuation), leave it unchanged.
             new_text += letter
     return new_text
+
+# print returned encoded/decoded message
+print(ceasar_cipher(coding_direction = direction, plain_text = text, shift_amount = shift))
