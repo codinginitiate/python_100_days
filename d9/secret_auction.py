d7a26f37e7cc029bd55bbc390a1cba5c62807427
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-22 20:01:53 +0000

    Thu, Feb 22, 2024, 8:01 PM +00:00

--- d9/secret_auction.py
+++ d9/secret_auction.py
@@ -4,13 +4,14 @@ def auction(bidders_name, bidders_bid):
 
     end_of_auction = False
     while not end_of_auction:
-        
+        name = input("Name: ")
+        amount = input("bid amount: ")
+        auction_dict[name]
 
 
 
 
-name = input("Name: ")
-amount = input("bid amount: ")
-auction(bidders_name = name, bidders_bid = amount)
+
+auction(bidders_name, bidders_bid)
 
 
