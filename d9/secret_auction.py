7af2820f698b091ac4d786ce4a59c8c8bcd6c491
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-22 20:08:34 +0000

    Thu, Feb 22, 2024, 8:08 PM +00:00

--- d9/secret_auction.py
+++ d9/secret_auction.py
@@ -1,16 +1,11 @@
 
 def auction(bidders_name, bidders_bid):
     auction_dict = {}
-    name = input("Name: ")
-    amount = input("bid amount: ")
     auction_dict[name] = amount
     return auction_dict
 
 
-
-
-
-
-print(auction(bidders_name, bidders_bid))
-
+name = input("Name: ")
+amount = input("bid amount: ")
+print(auction(name, amount))
 
