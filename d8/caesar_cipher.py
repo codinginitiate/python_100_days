5a77b2a27b2193167a7c8b1068ed8a59b46637d9
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-22 06:02:43 +0000

    Thu, Feb 22, 2024, 6:02 AM +00:00

--- d8/caesar_cipher.py
+++ d8/caesar_cipher.py
@@ -1,11 +1,14 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 new_text = "" # blank str to place decoded/encoded message.
+import art
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 # argument for parameter text_plain
 text = input("Type your message:\n").lower()
 # argument for parameter shift_amount
 shift = int(input("Type the shift number:\n"))
+print(art.logo)
+
 
 def ceasar_cipher(coding_direction, plain_text, shift_amount):
     new_text = ""
