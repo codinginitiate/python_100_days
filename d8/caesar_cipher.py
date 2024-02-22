3330b4362eb9edea8b14d2e25e6244361db80aa4
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-22 05:41:26 +0000

    Thu, Feb 22, 2024, 5:41 AM +00:00

--- d8/caesar_cipher.py
+++ d8/caesar_cipher.py
@@ -10,7 +10,7 @@ shift = int(input("Type the shift number:\n"))
 def ceasar_cipher(coding_direction, plain_text, shift_amount):
     new_text = ""
     if coding_direction == "decode":
-        shift_amount *+ -1
+        shift_amount *= -1
     for letter in plain_text:
         if letter in alphabet:
             index = alphabet.index(letter)
