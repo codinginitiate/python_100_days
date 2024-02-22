b26c5d0e8429f44d24f6e7d90a2baf55463192b3
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-22 13:50:08 +0000

    Thu, Feb 22, 2024, 1:50 PM +00:00

--- d8/caesar_cipher.py
+++ d8/caesar_cipher.py
@@ -1,9 +1,12 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 new_text = "" # blank str to place decoded/encoded message.
+
 from art import logo
 print(logo)
+
 should_end = False
 while not should_end:
+
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
     # argument for parameter text_plain
     text = input("Type your message:\n").lower()
@@ -14,15 +17,15 @@ while not should_end:
         new_text = ""
         if coding_direction == "decode":
             shift_amount *= -1 # used to change shift_amount to negative number, so only one line of code is needed at line 17
-        for letter in plain_text:
-            if letter in alphabet:
-                index = alphabet.index(letter)
+        for char in plain_text:
+            if char in alphabet:
+                index = alphabet.index(char)
                 new_index = (index + shift_amount) % 26
                 new_letter = alphabet[new_index]
                 new_text += new_letter
             else:
                 # If the character is not a letter (e.g., space or punctuation), leave it unchanged.
-                new_text += letter
+                new_text += char
         return new_text
 
     # print returned encoded/decoded message
