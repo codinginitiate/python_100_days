81f44fb526bee118ad38d5035d65eb5c27aba2c3
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-22 06:15:46 +0000

    Thu, Feb 22, 2024, 6:15 AM +00:00

--- d8/caesar_cipher.py
+++ d8/caesar_cipher.py
@@ -2,27 +2,28 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 new_text = "" # blank str to place decoded/encoded message.
 from art import logo
 print(logo)
-should
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-# argument for parameter text_plain
-text = input("Type your message:\n").lower()
-# argument for parameter shift_amount
-shift = int(input("Type the shift number:\n"))
+should_end = false
+while should_end:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    # argument for parameter text_plain
+    text = input("Type your message:\n").lower()
+    # argument for parameter shift_amount
+    shift = int(input("Type the shift number:\n"))
 
-def ceasar_cipher(coding_direction, plain_text, shift_amount):
-    new_text = ""
-    if coding_direction == "decode":
-        shift_amount *= -1 # used to change shift_amount to negative number, so only one line of code is needed at line 17
-    for letter in plain_text:
-        if letter in alphabet:
-            index = alphabet.index(letter)
-            new_index = (index + shift_amount) % 26
-            new_letter = alphabet[new_index]
-            new_text += new_letter
-        else:
-            # If the character is not a letter (e.g., space or punctuation), leave it unchanged.
-            new_text += letter
-    return new_text
+    def ceasar_cipher(coding_direction, plain_text, shift_amount):
+        new_text = ""
+        if coding_direction == "decode":
+            shift_amount *= -1 # used to change shift_amount to negative number, so only one line of code is needed at line 17
+        for letter in plain_text:
+            if letter in alphabet:
+                index = alphabet.index(letter)
+                new_index = (index + shift_amount) % 26
+                new_letter = alphabet[new_index]
+                new_text += new_letter
+            else:
+                # If the character is not a letter (e.g., space or punctuation), leave it unchanged.
+                new_text += letter
+        return new_text
 
-# print returned encoded/decoded message
-print(ceasar_cipher(coding_direction = direction, plain_text = text, shift_amount = shift ))
+    # print returned encoded/decoded message
+    print(ceasar_cipher(coding_direction = direction, plain_text = text, shift_amount = shift ))
