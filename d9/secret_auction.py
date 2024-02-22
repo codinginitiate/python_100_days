a57013c0043cd9e99cfaca720b8debcb8294820c
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-22 20:25:22 +0000

    Thu, Feb 22, 2024, 8:25 PM +00:00

--- d9/secret_auction.py
+++ d9/secret_auction.py
@@ -4,14 +4,15 @@ def auction(name, amount):
     return auction_dict
 
 
-
+auction_dict={}
 end_of_auction = False
 while not end_of_auction:
     name = input("Name: ")
     amount = input("bid amount: ")
+    auction_dict(name, amount)
     more_bidders = input("Are there more bidders?(yes or no) ")
     if more_bidders == 'no':
         end_of_auction = True
-        
+
 print(auction(name, amount))
 
