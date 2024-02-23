a3021d396533c6bfb9cfc4a58c1f476abc12acd1
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-23 21:17:46 +0000

    Fri, Feb 23, 2024, 9:17 PM +00:00

--- d10/calculator.py
+++ d10/calculator.py
@@ -21,4 +21,9 @@ def multiply(n1, n2):
 def divide(n1, n2):
     return n1 / n2
 
-math_dict = {+:add, -:subtract, *:multiply, /:divide}
+{
+    "+":add,
+    "-":subtract,
+    "*":multiply,
+    "/":divide
+}
