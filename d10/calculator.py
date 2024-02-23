fa32b6ece768e21dbf6a726b640bcb7bca8de5f8
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-23 20:11:02 +0000

    Fri, Feb 23, 2024, 8:11 PM +00:00

--- d10/calculator.py
+++ d10/calculator.py
@@ -8,8 +8,15 @@
 
 
 # Calculator
-
+#add
 def add(n1, n2):
     return n1 + n2
+#subtract
 def subtract(n1, n2):
     return n1-n2
+#multiply
+def multiply(n1, n2):
+    return n1 * n2
+#divide
+def divide(n1, n2):
+    return n1 / n2
