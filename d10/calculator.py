958c2e0503a72badbc8a9d85671b88062e4c788d
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-23 22:35:09 +0000

    Fri, Feb 23, 2024, 10:35 PM +00:00

--- d10/calculator.py
+++ d10/calculator.py
@@ -29,7 +29,7 @@ operations={
 
 num1 = int(input("What is the first number?: "))
 for key in operations:
-    print(key+", ", end ="")
+    print(key+" ", end ="")
 operation_symbol = input("Pick an operation symbol form the list above.: ")
 num2 = int(input("What is the second number?: "))
 
