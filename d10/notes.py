07ca6ca7e7dc3dfd679b2d87d51e22d0b91a9c18
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-23 19:56:25 +0000

    Fri, Feb 23, 2024, 7:56 PM +00:00

--- d10/notes.py
+++ d10/notes.py
@@ -5,3 +5,14 @@ def outer_function(z, x):
 
 result = outer_function(5, 10)
 print(result)
+
+
+def my_function(a):
+    if a < 40:
+        return
+        print("Terrible")
+    if a < 80:
+        return "Pass"
+    else:
+        return "Great"
+print(my_function(25))
