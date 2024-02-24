b16b8071d02818efd4b7208168b8367e3c40a3c9
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-24 16:16:39 +0000

    Sat, Feb 24, 2024, 4:16 PM +00:00

--- d10/calculator.py
+++ d10/calculator.py
@@ -28,49 +28,49 @@ operations={
 }
 
 def calculator():
-while True:
-    try:
-        num1 = int(input("What is the first number?: "))
-        break
-    except ValueError:
-        continue
-
-for key in operations:
-    print(key+" ", end ="")
-
-while True:
-    try:
-        operation_symbol = input("Pick an operation symbol form the list above.: ")
-        if operation_symbol in operations:
-            break
-    except ValueError:
-        continue
-
-should_continue = True
-
-while should_continue:
     while True:
         try:
-            num2 = int(input("What is the next number?: "))
+            num1 = int(input("What is the first number?: "))
             break
         except ValueError:
             continue
 
-    answer = main(num1, num2, operation_symbol)
-    print(f"{num1} {operation_symbol} {num2} = {answer}\n")
+    for key in operations:
+        print(key+" ", end ="")
 
-if input(f"Type 'y' to continue calculating with {answer} or type 'n' to exit: ") == 'y':
+    while True:
+        try:
+            operation_symbol = input("Pick an operation symbol form the list above.: ")
+            if operation_symbol in operations:
+                break
+        except ValueError:
+            continue
+
     should_continue = True
-    num1 = answer
-else:
-    should_continue = False
+
+    while should_continue:
+        while True:
+            try:
+                num2 = int(input("What is the next number?: "))
+                break
+            except ValueError:
+                continue
+
+        answer = main(num1, num2, operation_symbol)
+        print(f"{num1} {operation_symbol} {num2} = {answer}\n")
+
+    if input(f"Type 'y' to continue calculating with {answer} or type 'n' to exit: ") == 'y':
+        should_continue = True
+        num1 = answer
+    else:
+        should_continue = False
 
 
 
 
 
-if __name__=="__main__":
-    main(num1, num2, operation_symbol)
+    if __name__=="__main__":
+        main(num1, num2, operation_symbol)
 
 
 
