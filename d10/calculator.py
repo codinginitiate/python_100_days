f0bb3f5bfdf382f08d0b612fcbbb896c644a61d9
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-24 03:58:39 +0000

    Sat, Feb 24, 2024, 3:58 AM +00:00

--- d10/calculator.py
+++ d10/calculator.py
@@ -39,10 +39,10 @@ for key in operations:
 operation_symbol = input("\nPick an operation symbol form the list above.: ")
 while True:
     try:
-        num2 = int(input("What is the second number?: "))
+        num2 = int(input("What is the next number?: "))
     except ValueError:
         continue
-    
+
 answer = main(num1, num2, operation_symbol)
 print(f"{num1} {operation_symbol} {num2} = {answer}\n")
 
