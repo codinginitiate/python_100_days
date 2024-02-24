e447a4763b2b5ba9d0add7bb09aba473c65cdae4
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-24 23:31:09 +0000

    Sat, Feb 24, 2024, 11:31 PM +00:00

--- d10/calculator.py
+++ d10/calculator.py
@@ -57,7 +57,7 @@ def calculator():
         answer = operations[operation_symbol](num1, num2)
         print(f"{num1} {operation_symbol} {num2} = {answer}\n")
 
-        entry =input(f"Type 'y' to continue calculating with {answer}, type 'n' to start a new calculation, or 'e' to exit: ")
+        entry = input(f"Type 'y' to continue calculating with {answer}, type 'n' to start a new calculation, or 'e' to exit: ")
         if entry == 'y':
             num1 = answer
         elif entry == 'n':
