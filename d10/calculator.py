75abfc6eb27fe28fb74f8a3f0f61fe7fa0a58ffe
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-25 20:15:56 +0000

    Sun, Feb 25, 2024, 8:15 PM +00:00

--- d10/calculator.py
+++ d10/calculator.py
@@ -57,19 +57,19 @@ def calculator():
         answer = operations[operation_symbol](num1, num2)
         print(f"{num1} {operation_symbol} {num2} = {answer}\n")
 
-        try:
-            entry = input(f"Type 'y' to continue calculating with {answer}, type 'n' to start a new calculation, or 'e' to exit: ")
-            if entry == 'y':
-                num1 = answer
-            elif entry == 'n':
-                os.system("clear")
-                print(logo)
-                calculator()
-            elif entry == 'e':
-                should_continue = False
-            else:
-                print("Invalid input. Please choose 'y', 'n', or 'e'.")
-        except: ValueError
+
+        entry = input(f"Type 'y' to continue calculating with {answer}, type 'n' to start a new calculation, or 'e' to exit: ")
+        if entry == 'y':
+            num1 = answer
+        elif entry == 'n':
+            os.system("clear")
+            print(logo)
+            calculator()
+        elif entry == 'e':
+            break
+        else:
+            print("Invalid input. Please choose 'y', 'n', or 'e'.")
+
     os.system('clear')
 
 calculator()
