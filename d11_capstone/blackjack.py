e586c24f14203d3553d874030cba2c678c2fa2cb
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-26 16:02:26 +0000

    Mon, Feb 26, 2024, 4:02 PM +00:00

--- d11_capstone/blackjack.py
+++ d11_capstone/blackjack.py
@@ -3,4 +3,7 @@ input("Would you like to play a game? ('y'or'n'):  ")
 
 card_list= [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-def deal_cards = 
+def deal_cards():
+    card_list= [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    player_cards = []
+    for 
