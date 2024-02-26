64b501739a2c90b34b665ff551ceddbc952b9fe2
Jerry Thomas <49402611+codinginitiate@users.noreply.github.com>
2024-02-26 16:06:22 +0000

    Mon, Feb 26, 2024, 4:06 PM +00:00

--- d11_capstone/blackjack.py
+++ d11_capstone/blackjack.py
@@ -1,4 +1,4 @@
-
+import random
 input("Would you like to play a game? ('y'or'n'):  ")
 
 card_list= [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
@@ -6,4 +6,7 @@ card_list= [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 def deal_cards():
     card_list= [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     player_cards = []
-    for 
+    for i in 2:
+        player_cards = player_cards.append(random.choice.card_list)
+    print(player_cards)
+
